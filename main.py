b2b21440bbcd2ed77a5a233ab237ad9822cf4f70
DEVINx34 <devinx34@gmail.com>
2023-06-27 15:44:02 +0530

    giving the data after searching it in dataset

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -9,6 +9,12 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
+    file = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(file, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze()/10
+
+    return {"Date":date, "Station Id":station, "Temperature":temperature}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
